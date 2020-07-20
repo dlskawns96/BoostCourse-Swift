0fe592f5e19e00cbd6e2c23449f93813bba89da1
Nam Jun Lee <dlskawns96@gmail.com>
2020-07-20 18:57:48 +0900

    Music Player App

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Lee Nam Jun on 2020/07/20.
+//  Copyright © 2020 Lee NAMJUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
